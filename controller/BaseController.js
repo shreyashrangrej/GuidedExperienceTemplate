8e5f199803315157062b345d40bbe7f9c5f347b2
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2022-06-02 09:20:34 +0530

    Commit

--- controller/BaseController.js
+++ controller/BaseController.js
@@ -1,1 +1,73 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(t){"use strict";return t.extend("workzone2.workzonehr.sap.sf.workflow.returntoworkplace_employee.controller.BaseController",{getParameters:function(){return this.getOwnerComponent().getManifestEntry("sap.card").configuration.parameters},getI18nResource:function(){return this.getOwnerComponent().getModel("i18n").getResourceBundle()},isStringEmpty:function(t){return!t||!t.trim()},getGlobalModel:function(){return this.getOwnerComponent().getModel("global")},beforeStepComplete:function(){},beforeWizardComplete:function(){},embedWebsiteFeedbackStarter:function(t,e){let n="QSI_S_"+t.split("=")[1];var r=function(t,n,r,o){this.get=function(t){for(var t=t+"=",e=document.cookie.split(";"),n=0,r=e.length;n<r;n++){for(var o=e[n];" "==o.charAt(0);)o=o.substring(1,o.length);if(0==o.indexOf(t))return o.substring(t.length,o.length)}return null};this.set=function(t,e){var n="",n=new Date;n.setTime(n.getTime()+6048e5);n="; expires="+n.toGMTString();document.cookie=t+"="+e+n+"; path=/; "};this.check=function(){var e=this.get(r);if(e)e=e.split(":");else if(100!=t)"v"==n&&(t=Math.random()>=t/100?0:100),e=[n,t,0],this.set(r,e.join(":"));else return!0;var o=e[1];if(100==o)return!0;switch(e[0]){case"v":return!1;case"r":return o=e[2]%Math.floor(100/o),e[2]++,this.set(r,e.join(":")),!o}return!0};this.go=function(){if(this.check()){var t=document.createElement("script");t.type="text/javascript";t.src=o;document.getElementById(e)&&document.getElementById(e).appendChild(t)}};this.start=function(){var t=this;"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",function(){t.go()},!1):window.attachEvent&&window.attachEvent("onload",function(){t.go()}):t.go()}};try{new r(100,"r",n,t).start()}catch(t){}}})});
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (t) {
+    "use strict";
+    return t.extend("workzone2.workzonehr.sap.sf.workflow.returntoworkplace_employee.controller.BaseController", {
+        getParameters: function () {
+            return this.getOwnerComponent().getManifestEntry("sap.card").configuration.parameters
+        },
+        getI18nResource: function () {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle()
+        },
+        isStringEmpty: function (t) {
+            return !t || !t.trim()
+        },
+        getGlobalModel: function () {
+            return this.getOwnerComponent().getModel("global")
+        },
+        beforeStepComplete: function () {},
+        beforeWizardComplete: function () {},
+        embedWebsiteFeedbackStarter: function (t, e) {
+            let n = "QSI_S_" + t.split("=")[1];
+            var r = function (t, n, r, o) {
+                this.get = function (t) {
+                    for (var t = t + "=", e = document.cookie.split(";"), n = 0, r = e.length; n < r; n++) {
+                        for (var o = e[n];
+                            " " == o.charAt(0);) o = o.substring(1, o.length);
+                        if (0 == o.indexOf(t)) return o.substring(t.length, o.length)
+                    }
+                    return null
+                };
+                this.set = function (t, e) {
+                    var n = "",
+                        n = new Date;
+                    n.setTime(n.getTime() + 6048e5);
+                    n = "; expires=" + n.toGMTString();
+                    document.cookie = t + "=" + e + n + "; path=/; "
+                };
+                this.check = function () {
+                    var e = this.get(r);
+                    if (e) e = e.split(":");
+                    else if (100 != t) "v" == n && (t = Math.random() >= t / 100 ? 0 : 100), e = [n, t, 0], this.set(r, e.join(":"));
+                    else return !0;
+                    var o = e[1];
+                    if (100 == o) return !0;
+                    switch (e[0]) {
+                        case "v":
+                            return !1;
+                        case "r":
+                            return o = e[2] % Math.floor(100 / o), e[2]++, this.set(r, e.join(":")), !o
+                    }
+                    return !0
+                };
+                this.go = function () {
+                    if (this.check()) {
+                        var t = document.createElement("script");
+                        t.type = "text/javascript";
+                        t.src = o;
+                        document.getElementById(e) && document.getElementById(e).appendChild(t)
+                    }
+                };
+                this.start = function () {
+                    var t = this;
+                    "complete" !== document.readyState ? window.addEventListener ? window.addEventListener("load", function () {
+                        t.go()
+                    }, !1) : window.attachEvent && window.attachEvent("onload", function () {
+                        t.go()
+                    }) : t.go()
+                }
+            };
+            try {
+                new r(100, "r", n, t).start()
+            } catch (t) {}
+        }
+    })
+});
--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,1 +1,157 @@
-sap.ui.define(["sap/sf/workflow/returntoworkplace_employee/controller/BaseController","sap/m/WizardStep","sap/ui/core/mvc/XMLView","sap/ui/model/json/JSONModel","sap/ui/core/HTML"],function(e,t,i,s,a){"use strict";const o="Insertion---app--employeeLastPageSurveyLink";return e.extend("workzone2.workzonehr.sap.sf.workflow.returntoworkplace_employee.controller.Main",{onInit:async function(){this.card=this.getOwnerComponent().card;let e=this.getParameters().vaccineStepEnable.value;let s=this.byId("rtwMainFlowWizard");this.getGlobalModel().setProperty("/stepId",{});this.getGlobalModel().getProperty("/steps").forEach(a=>{if(!e&&a.id==="completeVaccineCheck"){return true}let o=new t(this.createId(a.id),a.settings);this.getGlobalModel().setProperty(`/stepId/${a.id}`,this.createId(a.id));o.attachComplete(this.completeWizardStep,this);o.cacheId=a.id;s.addStep(o);i.create({viewName:a.view}).then(e=>{e.cacheId=a.id;e.step=o;o.stepView=e;a.contentView=e;e.placeAt(o)})});this.updateFlowStateByCache();let n=new a;n.setContent(`<div id="${o}" />`);this.byId("insertionPosition").addItem(n)},uuid:-1,wfInstanceId:null,updateFlowStateByCache:async function(){let e=new s;e.setProperty("/canReturn",false);let t=this.byId("wizardStartButton");t.setModel(e,"btnModel");t.setBusy(true);let i=this.card.getCombinedParameters().loginUser;await this.card.request({url:`{{destinations.wfDestination}}/rest/v1/workflow-instances?definitionId=returnToWorkplaceEmployeeFlow&businessKey=${i}&$orderby=startedAt&$top=1&status=RUNNING,COMPLETED`,headers:{"Content-Type":"application/json"},withCredentials:true}).then(e=>{if(Array.isArray(e)&&e.length>0){if(e[0].status==="RUNNING"){this.wfInstanceId=e[0].id}else if(e[0].status==="COMPLETED"){this.byId("wizardNavContainer").to(this.byId("completedPage"))}}}).catch(e=>{this.loadError()});if(this.wfInstanceId){this.card.request({url:`{{destinations.wfDestination}}/rest/v1/workflow-instances/${this.wfInstanceId}/context`,headers:{"Content-Type":"application/json"},withCredentials:true}).then(i=>{this.uuid=i.uuid;if(!i.cache){i.cache={}}this.getGlobalModel().setProperty("/cache",i.cache);e.setProperty("/canReturn",true);t.setBusy(false)}).catch(e=>{this.loadError()})}else{t.setBusy(false)}},loadError:function(){let e=this.byId("loadError");let t=this.byId("startBox");e.setVisible(true);t.setVisible(false)},navigateToWizardContentPage:function(){this.byId("wizardNavContainer").to(this.byId("wizardContentPage"))},completeWizardStep:function(e){let t=e.getSource().stepView;t.getController().beforeStepComplete();this.getGlobalModel().setProperty("/cache/currentStep",e.getSource().cacheId);let i={workflowInstanceId:this.wfInstanceId,definitionId:"msgPayload",context:{uuid:this.uuid,tmpCache:this.getGlobalModel().getProperty("/cache")}};this.card.request({url:`{{destinations.wfDestination}}/rest/v1/messages`,headers:{"Content-Type":"application/json"},method:"POST",parameters:i,withCredentials:true})},completeWizard:function(){this.getGlobalModel().getProperty("/steps").forEach(e=>{let t=e.contentView;if(t){t.getController().beforeWizardComplete()}});let e={workflowInstanceId:this.wfInstanceId,definitionId:"msgPayload",context:{uuid:this.uuid,shutdown:true}};this.card.request({url:`{{destinations.wfDestination}}/rest/v1/messages`,headers:{"Content-Type":"application/json"},method:"POST",parameters:e,withCredentials:true});this.byId("wizardNavContainer").to(this.byId("completedPage"))},navToCompletePage:function(e){if(e.getParameters().to===this.byId("completedPage")){let e=this.getParameters();let t=e.lastPageQualtricsURL.value;if(t){window.QSI=undefined;this.renderQualtrics(t)}else{this.byId("DynamicSideContent").setShowSideContent(false)}}},renderQualtrics:function(e){setTimeout(()=>{this.embedWebsiteFeedbackStarter(e,o)},200)}})});
+sap.ui.define(["sap/sf/workflow/returntoworkplace_employee/controller/BaseController", "sap/m/WizardStep", "sap/ui/core/mvc/XMLView", "sap/ui/model/json/JSONModel", "sap/ui/core/HTML"], function (e, t, i, s, a) {
+    "use strict";
+    const o = "Insertion---app--employeeLastPageSurveyLink";
+    return e.extend("workzone2.workzonehr.sap.sf.workflow.returntoworkplace_employee.controller.Main", {
+        onInit: async function () {
+            this.card = this.getOwnerComponent().card;
+            let e = this.getParameters().vaccineStepEnable.value;
+            let s = this.byId("rtwMainFlowWizard");
+            this.getGlobalModel().setProperty("/stepId", {});
+            this.getGlobalModel().getProperty("/steps").forEach(a => {
+                if (!e && a.id === "completeVaccineCheck") {
+                    return true
+                }
+                let o = new t(this.createId(a.id), a.settings);
+                this.getGlobalModel().setProperty(`/stepId/${a.id}`, this.createId(a.id));
+                o.attachComplete(this.completeWizardStep, this);
+                o.cacheId = a.id;
+                s.addStep(o);
+                i.create({
+                    viewName: a.view
+                }).then(e => {
+                    e.cacheId = a.id;
+                    e.step = o;
+                    o.stepView = e;
+                    a.contentView = e;
+                    e.placeAt(o)
+                })
+            });
+            this.updateFlowStateByCache();
+            let n = new a;
+            n.setContent(`<div id="${o}" />`);
+            this.byId("insertionPosition").addItem(n)
+        },
+        uuid: -1,
+        wfInstanceId: null,
+        updateFlowStateByCache: async function () {
+            let e = new s;
+            e.setProperty("/canReturn", false);
+            let t = this.byId("wizardStartButton");
+            t.setModel(e, "btnModel");
+            t.setBusy(true);
+            let i = this.card.getCombinedParameters().loginUser;
+            await this.card.request({
+                url: `{{destinations.wfDestination}}/rest/v1/workflow-instances?definitionId=returnToWorkplaceEmployeeFlow&businessKey=${i}&$orderby=startedAt&$top=1&status=RUNNING,COMPLETED`,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true
+            }).then(e => {
+                if (Array.isArray(e) && e.length > 0) {
+                    if (e[0].status === "RUNNING") {
+                        this.wfInstanceId = e[0].id
+                    } else if (e[0].status === "COMPLETED") {
+                        this.byId("wizardNavContainer").to(this.byId("completedPage"))
+                    }
+                }
+            }).catch(e => {
+                this.loadError()
+            });
+            if (this.wfInstanceId) {
+                this.card.request({
+                    url: `{{destinations.wfDestination}}/rest/v1/workflow-instances/${this.wfInstanceId}/context`,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }).then(i => {
+                    this.uuid = i.uuid;
+                    if (!i.cache) {
+                        i.cache = {}
+                    }
+                    this.getGlobalModel().setProperty("/cache", i.cache);
+                    e.setProperty("/canReturn", true);
+                    t.setBusy(false)
+                }).catch(e => {
+                    this.loadError()
+                })
+            } else {
+                t.setBusy(false)
+            }
+        },
+        loadError: function () {
+            let e = this.byId("loadError");
+            let t = this.byId("startBox");
+            e.setVisible(true);
+            t.setVisible(false)
+        },
+        navigateToWizardContentPage: function () {
+            this.byId("wizardNavContainer").to(this.byId("wizardContentPage"))
+        },
+        completeWizardStep: function (e) {
+            let t = e.getSource().stepView;
+            t.getController().beforeStepComplete();
+            this.getGlobalModel().setProperty("/cache/currentStep", e.getSource().cacheId);
+            let i = {
+                workflowInstanceId: this.wfInstanceId,
+                definitionId: "msgPayload",
+                context: {
+                    uuid: this.uuid,
+                    tmpCache: this.getGlobalModel().getProperty("/cache")
+                }
+            };
+            this.card.request({
+                url: `{{destinations.wfDestination}}/rest/v1/messages`,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                parameters: i,
+                withCredentials: true
+            })
+        },
+        completeWizard: function () {
+            this.getGlobalModel().getProperty("/steps").forEach(e => {
+                let t = e.contentView;
+                if (t) {
+                    t.getController().beforeWizardComplete()
+                }
+            });
+            let e = {
+                workflowInstanceId: this.wfInstanceId,
+                definitionId: "msgPayload",
+                context: {
+                    uuid: this.uuid,
+                    shutdown: true
+                }
+            };
+            this.card.request({
+                url: `{{destinations.wfDestination}}/rest/v1/messages`,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                parameters: e,
+                withCredentials: true
+            });
+            this.byId("wizardNavContainer").to(this.byId("completedPage"))
+        },
+        navToCompletePage: function (e) {
+            if (e.getParameters().to === this.byId("completedPage")) {
+                let e = this.getParameters();
+                let t = e.lastPageQualtricsURL.value;
+                if (t) {
+                    window.QSI = undefined;
+                    this.renderQualtrics(t)
+                } else {
+                    this.byId("DynamicSideContent").setShowSideContent(false)
+                }
+            }
+        },
+        renderQualtrics: function (e) {
+            setTimeout(() => {
+                this.embedWebsiteFeedbackStarter(e, o)
+            }, 200)
+        }
+    })
+});
